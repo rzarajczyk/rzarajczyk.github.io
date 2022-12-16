76d127576efc6c1744e339df2920293322e10e53
Rafał Zarajczyk <rzarajczyk@users.noreply.github.com>
2022-12-16 11:51:55 +0100

    Update macos-install-java.sh

--- resources/macos-install-java.sh
+++ resources/macos-install-java.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 brew install openjdk@11
 brew install openjdk@17
-sudo ln -sfn /usr/local/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
-sudo ln -sfn /usr/local/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-17.jdk
+sudo ln -sfn $(brew --prefix openjdk@11)/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
+sudo ln -sfn $(brew --prefix openjdk@17)/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-17.jdk
 echo 'alias java11="export JAVA_HOME=$(/usr/libexec/java_home -v 11); java -version"' >> ~/.zshrc
 echo 'alias java17="export JAVA_HOME=$(/usr/libexec/java_home -v 17); java -version"' >> ~/.zshrc
